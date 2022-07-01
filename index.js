bc60ef6c321f58a9c82a54ca4841a0a81598c516
rabiul1399 <www.rabiulislam3808@gamil.com>
2022-07-01 21:19:36 +0600

    second comet

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 // Middleware
@@ -20,7 +20,6 @@ async function run() {
 
         app.post('/task', async (req, res) => {
             const newTask = req.body;
-            console.log('adding new data', newTask)
             const result = await taskCollection.insertOne(newTask);
             res.send(result)
         })
@@ -29,9 +28,33 @@ async function run() {
             const users = await taskCollection.find().toArray();
             res.send(users)
         })
+        app.get('/task/:id',async(req,res) =>{
+            const id=req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result= await taskCollection.findOne(query)
+            res.send(result)
+            
+        })
+
+        app.put('/task/:id',async(req,res) =>{
+            const id=req.params.id;
+            const updateTask = req.body;
+            console.log(updateTask)
+            const filter = {_id: ObjectId(id)}
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    title: updateTask.title,
+                    details: updateTask.details,
+                    time: updateTask.time
+                },
+              };
+              const result = await taskCollection.updateOne(filter, updateDoc, options);
+              res.send(result);
+        })
     }
     finally {
-        // await client.close();
+     
     }
 }
 
